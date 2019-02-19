72611669b014901282cb311890ab5e31b13d1b27
LeandroMelloo <leandromoreno1909@hotmail.com>
2019-02-19 17:02:31 -0300

    criação do custom-express

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const http = require('http');
+const app = require('./src/config/custom-express')
 
-const servidor = http.createServer(function (req, res) {
-    let html = '';
+app.listen(3000, function () {
+    console.log('Servidor rodando na porta 3000')
+})
 
-    if (req.url == '/') {
-        html = `
+app.get('/', function (req, res) {
+    res.send(`
         <html>
             <head>
                 <meta charset="utf-8">
@@ -13,21 +14,18 @@ const servidor = http.createServer(function (req, res) {
                 <h1>Casa do Livro</h1>
             </body>
         </html>
-        `; 
-    } else if (req.url == '/livros') {
-        html = `
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1>Listagem de Livros</h1>
-            </body>
-        </html>
-        `;
-    }
+    `)
+})
 
-    res.end(html);
-});
-
-servidor.listen(3000);
+app.get('/livros', function (req, res) {
+    res.send(`
+    <html>
+        <head>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1>Listagem de Livros</h1>
+        </body>
+    </html>
+    `)
+})
