d98a76daab80ce36cb9243f58252741332f8ae01
LeandroMelloo <leandromoreno1909@hotmail.com>
2019-02-19 13:04:13 -0300

    criação do server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+
+const servidor = http.createServer(function (req, res) {
+    let html = '';
+
+    if (req.url == '/') {
+        html = `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Casa do Livro</h1>
+            </body>
+        </html>
+        `; 
+    } else if (req.url == '/livros') {
+        html = `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Listagem de Livros</h1>
+            </body>
+        </html>
+        `;
+    }
+
+    res.end(html);
+});
+
+servidor.listen(3000);
