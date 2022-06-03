7506476ff42f95b8fb3938db94052fbd918c72c7
pedromellodev <pedromello19122006@gmail.com>
2022-06-03 20:37:25 -0300

    curso Node

---
+++ tipo_numero.js
@@ -0,0 +1,18 @@
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const somar = primeiroNumero + segundoNumero;
+const multiplicar = primeiroNumero * segundoNumero;
+const subtrair = primeiroNumero - segundoNumero;
+const divisao = primeiroNumero / segundoNumero;
+
+console.log(somar);
+console.log(multiplicar);
+console.log(subtrair);
+console.log(divisao);
+
+// Ponto Flutuante
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
